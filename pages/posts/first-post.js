73ff46d7d73f6aba77cb645ce25fcb0f2c91adca
Alex <vanguard88728@gmail.com>
2023-02-26 03:21:59 +0800

    Update css

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,12 +1,17 @@
 import Link from 'next/link';
+import Head from 'next/head';
+import Layout from '../../components/layout';
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
       <h1>First post</h1>
       <h2>
         <Link href="/">Back to Home</Link>
       </h2>
-    </>
+    </Layout>
   );
 }
